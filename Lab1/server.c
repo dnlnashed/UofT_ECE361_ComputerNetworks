8b68215317f1c95491a2e604a99f1b9a65481ecc
Daniel Nashed <daniel.nashed@mail.utoronto.ca>
2022-09-25 17:54:03 -0400

    Socket checks for message, gives specific response

--- Lab1/server.c
+++ Lab1/server.c
@@ -4,22 +4,36 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
     //lines 10-20 follow the same format of code as in Beej's guide
     int sockfd;
     struct sockaddr_in my_addr;
+    struct sockaddr_storage their_addr;
+    char buf[100];
+    char yes[3] = "yes";
+    char no[2] = "no";
+
+    socklen_t addr_len;
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(atoi(*argv));
+    my_addr.sin_port = htons(atoi(argv[1]));
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     memset(my_addr.sin_zero,'\0', sizeof(my_addr.sin_zero));
 
     int bind_check = bind(sockfd , (struct sockaddr *)&my_addr, sizeof my_addr);
 
+    addr_len = sizeof their_addr;
+    recvfrom(sockfd, buf, 99, 0, (struct sockaddr *)&their_addr, &addr_len);
 
+    if(buf == "ftp"){
+        sendto(sockfd, yes, sizeof yes, 0, (struct sockaddr *)&their_addr, sizeof their_addr);
+    } else {
+        sendto(sockfd, no, sizeof no, 0, (struct sockaddr *)&their_addr, sizeof their_addr);
+    }
 
     return 0;
 }
